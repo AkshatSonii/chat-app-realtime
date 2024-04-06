cca8bbb06ea17ad222c8a6f8a6e5b06ea022d782
AkshatSonii <akshatsoni450@gmail.com>
2024-04-06 12:43:49 +0530

    change request url

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "http://localhost:5000/api/user/login",
+        "https://chatify-z15s.onrender.com/api/user/login",
         { email, password },
         config
       );
